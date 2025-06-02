0ad4c06609b9ea3febeaf86fdc705ab4c7e06f89
Sheltone Swaka <sheltoneswaka@gmail.com>
2025-06-02 13:34:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+<html>
+ <header>
+    <title>TEEFAN MUSIC</title>
+    <link rel="stylesheet" href="./style.css"></link>
+ </header>
+ <body>
+    <div class="centered-heading">
+        <p><a href="./home.html">Home</a></p>
+        <p><a href="./about.html">About</a></p>
+        <p><a href="./booking.html">Booking</a></p>
+        <p><a href="./contact.html">Contact</a></p>
+    </div>
+    <div class="background">
+    </div>
+    
+ </body>
+</html>
